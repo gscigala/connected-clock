4cd1c058ce2641b366275b81776c5902551f5836
Guillaume Scigala <g.scigala@startmail.com>
2018-02-03 17:40:39 +0100

    Initial Commit

---
+++ BigBen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BigBen
+//
+//  Created by Guillaume Scigala on 03/02/2018.
+//  Copyright © 2018 Silence. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
